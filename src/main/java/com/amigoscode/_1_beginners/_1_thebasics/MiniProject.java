1a01e05e10c278214e0170c3c27924b372b2e59e
moazzam <mzzmsameer@gmail.com>
2026-09-19 10:19:25 +0530

    fourth  commit to git. completed beginner Exercises. MiniProject, Operators, Scanner, String and Variables.

--- src/main/java/com/amigoscode/_1_beginners/_1_thebasics/MiniProject.java
+++ src/main/java/com/amigoscode/_1_beginners/_1_thebasics/MiniProject.java
@@ -14,6 +14,7 @@ public class MiniProject {
     public static void main(String[] args) {
 
         // TODO: 1 - Create a Scanner for user input
+        Scanner scanner = new Scanner(System.in);
 
 
         // TODO: 2 - Display a menu with the following options:
@@ -25,13 +26,47 @@ public class MiniProject {
         //   "4. Divide"
         //   "Choose an operation (1-4): "
 
+        System.out.print( "=== Simple Calculator ===" +
+                " 1. Add" +
+                " 2. Subtract" +
+                " 3. Multiply" +
+                " 4. Divide" +
+                " Choose an operation (1-4): ");
+
 
         // TODO: 3 - Read the user's choice into an int variable
+        int choice = scanner.nextInt();
 
 
         // TODO: 4 - Prompt and read two double numbers from the user
         // Print "Enter first number: " and read it.
         // Print "Enter second number: " and read it.
+        System.out.println("Enter the first number: ");
+        double firstNumber = scanner.nextDouble();
+        System.out.println("Enter the second number: ");
+        double secondNumber = scanner.nextDouble();
+
+            if (choice == 1) {
+                System.out.println("Sum: " + (firstNumber + secondNumber));
+            } else if (choice == 2) {
+
+                System.out.print("Difference: " +( firstNumber - secondNumber));
+
+            }else if (choice == 3) {
+
+                System.out.print("Product:  " + (firstNumber * secondNumber));
+            }else if (choice == 4) {
+                if (secondNumber == 0) {
+                    System.out.println("cannot divide by zero");
+                }else {
+                    System.out.print("quotient:  " + (firstNumber/ secondNumber));
+                }
+
+            }
+
+
+
+
 
 
         // TODO: 5 - Use a switch statement to perform the chosen operation and print the result
